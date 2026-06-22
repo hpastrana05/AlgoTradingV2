15d7d6bc5e14f19044432a211f4faa696209ebfa
hpastrana <hugo.pastrana.torres@gmail.com>
2026-06-22 20:25:06 +0200

    added run() to TradingEngine class

--- classes/trading_engine.py
+++ classes/trading_engine.py
@@ -1,5 +1,12 @@
+import logging
+from datetime import datetime, timedelta
+import time
+
 from .strategy_manager import StrategyManager
 from .broker_sync_manager import BrokerSyncManager
+
+LOGGER = logging.getLogger("TradingEngine")
+
 
 class TradingEngine:
 
@@ -13,3 +20,35 @@ class TradingEngine:
     def check_trading_strategy(self):
         ticker, action, price = self.strategy_manager.check_strategy()
         self.broker_sync_manager.process_actions(ticker, action, price)
+    
+
+    def run(self):
+        while True:
+            sleep_until_next_interval(minutes=1, offset_seconds=1)
+
+            LOGGER.debug(f"Running Strategy at {datetime.now()}")
+            self.update_market_data()
+            self.check_trading_strategy()
+    
+
+
+def sleep_until_next_interval(hours=0, minutes=0, seconds=0, offset_seconds=0):
+    interval = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+
+    if interval.total_seconds() <= 0:
+        LOGGER.error("Interval must be greater than 0 seconds.")
+        raise ValueError("Interval must be greater than 0 seconds.")
+
+    now = datetime.now()
+    interval_seconds = int(interval.total_seconds())
+
+    current_ts = now.timestamp()
+    next_ts = ((int(current_ts) // interval_seconds) + 1) * interval_seconds + offset_seconds
+
+    next_run = datetime.fromtimestamp(next_ts)
+
+    if next_run <= now:
+        next_run += interval
+
+    sleep_seconds = (next_run - now).total_seconds()
+    time.sleep(max(sleep_seconds, 0))
--- main.py
+++ main.py
@@ -22,27 +22,6 @@ logging.getLogger("Signals").setLevel(logging.DEBUG)
 
 LOGGER = logging.getLogger("Main")
 
-def sleep_until_next_interval(hours=0, minutes=0, seconds=0, offset_seconds=0):
-    interval = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-
-    if interval.total_seconds() <= 0:
-        LOGGER.error("Interval must be greater than 0 seconds.")
-        raise ValueError("Interval must be greater than 0 seconds.")
-
-    now = datetime.now()
-    interval_seconds = int(interval.total_seconds())
-
-    current_ts = now.timestamp()
-    next_ts = ((int(current_ts) // interval_seconds) + 1) * interval_seconds + offset_seconds
-
-    next_run = datetime.fromtimestamp(next_ts)
-
-    if next_run <= now:
-        next_run += interval
-
-    sleep_seconds = (next_run - now).total_seconds()
-    time.sleep(max(sleep_seconds, 0))
-
 
 def main():
     LOGGER.info("Starting the trading engine...")
@@ -50,14 +29,8 @@ def main():
     strat_path = "strategies/strat_2.json"
 
     trading_engine = TradingEngine(strat_path)
-    
-    while True:
-        
-        sleep_until_next_interval(minutes=1, offset_seconds=1)
 
-        LOGGER.debug(f"Running Strategy at {datetime.now()}")
-        trading_engine.update_market_data()
-        trading_engine.check_trading_strategy()
+    trading_engine.run()
 
 
 if __name__ == "__main__":
